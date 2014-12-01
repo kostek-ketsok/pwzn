d83d18b1ff423ddc8484e22acaf0b797943f9736
kostek <kostek-ketsok@users.noreply.github.com>
2014-12-01 01:48:39 +0100

    5.3 - nadal histgramy nie dzialają

--- tasks/zaj5/zadanie3.py
+++ tasks/zaj5/zadanie3.py
@@ -31,8 +31,8 @@ def get_center_of_mass(event_id, data):
     indeksy = np.where(event_table == event_id)
     masa_kolumna = np.array(data['particle_mass'])
     polozenia_kolumna = np.array(data['particle_position'])
-    masa = np.zeros((np.max(indeksy)+1))
-    polozenia = np.zeros((np.max(indeksy)+1, 3))
+    masa = np.zeros((np.max(indeksy)-np.min(indeksy)+1))
+    polozenia = np.zeros((np.max(indeksy)-np.min(indeksy)+1, 3))
     it = 0
     for i in indeksy[0]:
         polozenia[it, 0] = polozenia_kolumna[i, 0]
@@ -41,14 +41,12 @@ def get_center_of_mass(event_id, data):
         masa[it] = masa_kolumna[i]
         it += 1
         #print(it-1, polozenia[it-1], polozenia_kolumna[i], masa[it-1], masa_kolumna[i])
-    
     #polozenia = np.array(data['particle_position'])
     #masa = np.array(data['particle_mass'])
     masa_calkowita = masa.sum(axis=0)
     polozenie_suma = np.zeros((len(polozenia[:,0]), 3))
     polozenie_suma = polozenia[:, :] * masa[:, np.newaxis]
     polozenie_suma2 = polozenie_suma.sum(axis=0, dtype=np.dtype('f'))
-    
     #for i in range(0,1000):
         #print(i, polozenia[i], masa[i], polozenie_suma[i])
     #print(masa)
@@ -69,21 +67,36 @@ def get_energy_spectrum(event_id, data, left, right, bins):
 
     Podpowiedż: np.histogram
     """
-    v = data['particle_velocity'][:]
-    m = data['particle_mass'][:]
-    v2 = v[:, 0]*v[:, 0]
-    v2_dlugosc = v2[:,0]*v2[:,0] + v2[:,1]*v2[:,1] + v2[:,2]*v2[:,2]
-    ekin = 0.5 * v2_dlugosc[:, np.newaxis] * m[:]
+    event_table = data['event_id']
+    indeksy = np.where(event_table == event_id)
+    m_kolumna = np.array(data['particle_mass'])
+    v_kolumna = np.array(data['particle_velocity'])
+    #print(event_table, np.max(indeksy)-np.min(indeksy))
+    m = np.zeros((np.max(indeksy)-np.min(indeksy)+1))
+    v = np.zeros((np.max(indeksy)-np.min(indeksy)+1, 3))
+    it = 0
+    for i in indeksy[0]:
+        v[it, 0] = v_kolumna[i, 0]
+        v[it, 1] = v_kolumna[i, 1]
+        v[it, 2] = v_kolumna[i, 2]
+        m[it] = m_kolumna[i]
+        it += 1
+        #print(it-1, v[it-1], v_kolumna[i], m[it-1], m[i])
 
-    #print("ekin=", ekin,"\nmasa=", m[0:3], "\nv2=",v2_dlugosc[:])
-    ekin2 = []
+    #v = data['particle_velocity'][:]
+    #m = data['particle_mass'][:]
+    v2 = v[:, 0]*v[:, 0] + v[:, 1]*v[:, 1] + v[:, 2]*v[:, 2]
+    ekin = 0.5 * v2[:] * m[:]
+
+    #print("ekin=", ekin[0:3],"\nmasa=", m[0:3], "\nv2=",v2[0:3], "\nv=",v[0:3, 0:3])
+    #print(ekin)
+    ekin_lista = []
     for i in ekin:
-        if i[0] > left:
-            if i[0] < right:
-                ekin2.append(i[0])
-    ekin2 = sorted(ekin, key=lambda x: x[0])
-    wartosci, biny = np.histogram(ekin2, bins)
-    #print(wartosci)
+        if (i > left) & (i < right):
+            ekin_lista.append(i)
+    #ekin_lista = sorted(ekin, key=lambda x: x, reverse=True)
+    wartosci, biny = np.histogram(ekin_lista, bins)
+    #print(ekin_lista)
     return wartosci
     
 
